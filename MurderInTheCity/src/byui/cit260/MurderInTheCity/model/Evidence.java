0c98ca3f9a709cae697c1f8fa667b90151be4a95
Luiz Bezerra <>
2016-01-30 19:20:45 -0200

    01/30/2016 - Game, Map, Location, DefenseAttorneyOffice, Crime, CrimeScene, and Evidence classes created

---
+++ MurderInTheCity/src/byui/cit260/MurderInTheCity/model/Evidence.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.MurderInTheCity.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Luiz
+ */
+public class Evidence implements Serializable {
+    
+    // Class instance variables
+    private String description;
+    private String locationCollected;
+
+    @Override
+    public String toString() {
+        return "Evidence{" + "description=" + description + ", locationCollected=" + locationCollected + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.description);
+        hash = 79 * hash + Objects.hashCode(this.locationCollected);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Evidence other = (Evidence) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.locationCollected, other.locationCollected)) {
+            return false;
+        }
+        return true;
+    }
+
+    public Evidence() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocationCollected() {
+        return locationCollected;
+    }
+
+    public void setLocationCollected(String locationCollected) {
+        this.locationCollected = locationCollected;
+    }
+}
--- MurderInTheCity/src/murderinthecity/MurderInTheCity.java
+++ MurderInTheCity/src/murderinthecity/MurderInTheCity.java
@@ -6,10 +6,17 @@
 package murderinthecity;
 
 import byui.cit260.MurderInTheCity.model.Player;
+import byui.cit260.MurderInTheCity.model.Game;
+import byui.cit260.MurderInTheCity.model.Map;
+import byui.cit260.MurderInTheCity.model.Location;
+import byui.cit260.MurderInTheCity.model.DefenseAttorneyOffice;
+import byui.cit260.MurderInTheCity.model.Crime;
+import byui.cit260.MurderInTheCity.model.CrimeScene;
+import byui.cit260.MurderInTheCity.model.Evidence;
 
 /**
  *
- * @author Daniel
+ * @author Daniel and Luiz
  */
 public class MurderInTheCity {
 
@@ -17,6 +24,11 @@ public class MurderInTheCity {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        // Test string for classes
+        String classInfo;
+
+        // Testing the Player class
         Player player = new Player();
         
         player.setName("Daniel Eckton");
@@ -24,8 +36,76 @@ public class MurderInTheCity {
         player.setCrimesSolved(5);
         player.setTimeAsDetective(3);
         
-        String playerInfo = player.toString();
-        System.out.println(playerInfo);
+        classInfo = player.toString();
+        System.out.println(classInfo);
+        
+        // Testing the Game class
+        Game game = new Game();
+        
+        game.setSavedGame(2);
+        
+        classInfo = game.toString();
+        System.out.println(classInfo);
+        
+        // Testing the Map class
+        Map map = new Map();
+        
+        map.setLocationCount(7);
+        
+        classInfo = map.toString();
+        System.out.println(classInfo);
+        
+        // Testing the Location class
+        Location location = new Location();
+        
+        location.setVisited(true);
+        location.setName("Zoo Manager Office");
+        location.setDescription("The victim boss' office.");
+        location.setDistanceFromCrime(2.9);
+        location.setParentLocation("City Zoo");
+        
+        classInfo = location.toString();
+        System.out.println(classInfo);
+        
+        // Testing the DefenseAttorneyOffice class
+        DefenseAttorneyOffice office = new DefenseAttorneyOffice();
+        
+        office.setSolve(false);
+        office.setEvidence("Fingerprints");
+        office.setSuspect("Sterling Meyers");
+        
+        classInfo = office.toString();
+        System.out.println(classInfo);
+        
+        // Testing the Crime class
+        Crime crime = new Crime();
+        
+        crime.setCrimeType("Murder");
+        crime.setCrimePlace("Pet Shop");
+        crime.setCrimeVictim("Luiz Bezerra");
+        crime.setCrimeOffender("Unknown");
+        crime.setSolved(false);
+        
+        classInfo = crime.toString();
+        System.out.println(classInfo);
+        
+        // Testing the CrimeScene class
+        CrimeScene crimeScene = new CrimeScene();
+        
+        crimeScene.setWitnesses(2);
+        crimeScene.setTemperature(76.9);
+        
+        classInfo = crimeScene.toString();
+        System.out.println(classInfo);
+        
+        // Testing the Evidence class
+        Evidence evidence = new Evidence();
+        
+        evidence.setDescription("Fingerprints found on the murder weapon");
+        evidence.setLocationCollected("Dumpster near the crime scene");
+        
+        classInfo = evidence.toString();
+        System.out.println(classInfo);
     }
     
 }
